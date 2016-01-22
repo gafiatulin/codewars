bfbb8f6bf1f6864c7dbe9f7d5c8ab7b76abbf247
Victor Gafiatulin <gafiatulin@gmail.com>
2016-01-23 00:28:07 +0300

    added solution to "Dice rolls threshold"

---
+++ src/Beta/Dice.hs
@@ -0,0 +1,15 @@
+-- Dice rolls threshold
+-- http://www.codewars.com/kata/55d18ceefdc5aba4290000e5/
+
+module Dice (rollDice) where
+
+import Data.Array (array, Array, (!))
+
+rollDice :: Integer -> Integer -> Integer -> Double
+rollDice rolls sides threshold = (/ fromIntegral (sides^rolls)) . sum $ [j | (i,j) <- distributions ! (rolls, sides), i >= threshold]
+
+distributions = array ((0,0), (100, 100)) [ ((j, i), f j i) | i <- [0..100], j <- [0..100]]
+    where f rolls sides = map (\s -> (s, sum . map (\k -> (-1)^k * binomial rolls k * binomial (s-k*sides-1) (rolls-1)) $ [0..(s-rolls) `div` sides])) [rolls .. sides*rolls]
+          binomial n k = coeffs !! fromIntegral n !! fromIntegral k
+          coeffs = iterate next [1] 
+              where next ns = zipWith (+) (0:ns) $ ns ++ [0]
